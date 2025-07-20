48f0f9dea754b61dd9838c4a5b781f2d60265ec5
Muhammad Hammad145 <h.mern.developer@gmail.com>
2025-07-20 15:42:57 +0500

    Updated code after some changes

--- src/pages/Frontend/Home/OfferTime.jsx
+++ src/pages/Frontend/Home/OfferTime.jsx
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import { Card, Col, Image, Row, Typography } from 'antd';
 import photo from "../../../assets/hero section.png"
-// import photo from "../../assets/hero section.png"
+
 
 
 
@@ -85,32 +85,7 @@ const OfferTimer = () => {
                                     </Card>
                                 </Col>
                             </Row>
-                            {/* <Row gutter={[16, 16]} justify="center">
-                                <Col xs={6}>
-                                    <Card className='card' style={{ textAlign: 'center' }}>
-                                        <Title level={5}>{timeLeft.days}</Title>
-                                        <p>Days</p>
-                                    </Card>
-                                </Col>
-                                <Col xs={6}>
-                                    <Card style={{ textAlign: 'center' }}>
-                                        <Title level={5}>{timeLeft.hours}</Title>
-                                        <p>Hours</p>
-                                    </Card>
-                                </Col>
-                                <Col xs={6}>
-                                    <Card style={{ textAlign: 'center' }}>
-                                        <Title level={5}>{timeLeft.minutes}</Title>
-                                        <p>Minutes</p>
-                                    </Card>
-                                </Col>
-                                <Col xs={6}>
-                                    <Card style={{ textAlign: 'center' }}>
-                                        <Title level={5}>{timeLeft.seconds}</Title>
-                                        <p>Seconds</p>
-                                    </Card>
-                                </Col>
-                            </Row> */}
+                           
                         </div>
                     </Col>
 
@@ -122,54 +97,7 @@ const OfferTimer = () => {
             </div>
         </main>
 
-        // <main>
-
-        // <div className='container mt-5 '>
-        //     <Row gutter={16} className='d-flex justify-content-between'>
-        //         <Col span={12} >
-        //             <Title level={2}>Deals of this Month</Title>
-        //             <Paragraph>Bigs sale of this month bug it and make fun it.It's new stock not.
-        //                 purchase it and enjoy it.
-        //             </Paragraph>
-        //             <Row gutter={16}>
-        //             <Col span={12} className='d-flex d-row'>
-
-
-        //            <Col span={4} ><Card.Grid style={{width:150,height:100}}className="card d-flex d-colume align-items-center  shadow p-3 mb-5 bg-body-tertiary rounded"  >
-        //             <Title level={3}>{timeLeft.days}</Title> <br/>
-        //             <Paragraph>Days</Paragraph>
-        //              </Card.Grid>
-        //            </Col>
-
-        //            <Col span={4} ><Card.Grid style={{width:150,height:100}}className="card d-flex d-colume align-items-center  shadow p-3 mb-5 bg-body-tertiary rounded"  >
-        //             <Title level={3}>{timeLeft.hours}</Title> <br/>
-        //             <Paragraph>Hours</Paragraph>
-        //              </Card.Grid>
-        //            </Col>
-
-        //            <Col span={4} ><Card.Grid style={{width:150,height:100}}className="card d-flex d-colume align-items-center  shadow p-3 mb-5 bg-body-tertiary rounded"  >
-        //             <Title level={3}>{timeLeft.minutes}</Title> <br/>
-        //             <Paragraph>Minutes</Paragraph>
-        //              </Card.Grid>
-        //            </Col>
-
-        //            <Col span={4} ><Card.Grid style={{width:"",height:100}}className="card d-flex d-colume align-items-center  shadow p-3 mb-5 bg-body-tertiary rounded"  >
-        //             <Title level={3}>{timeLeft.seconds}</Title> <br/>
-        //             <Paragraph>Seconds</Paragraph>
-        //              </Card.Grid>
-        //            </Col>
-
-
-        //             </Col>
-        //             </Row>
-
-        //         </Col>
-        //         <Col span={12}>
-        //                 <Image preview="none"  src={photo} className='w-100'/>
-        //         </Col>
-        //     </Row>
-        // </div>
-        // </main>
+      
 
     );
 };
